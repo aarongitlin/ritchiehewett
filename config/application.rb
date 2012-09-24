cff14ee3598f7c5a3515ac469330437688addba3
Aaron Gitlin <aarongitlin@gmail.com>
2012-09-24 23:09:48 +1000

    changes for heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module RitchieHewett
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+		# precompile assets for heroku
+		config.assets.initialize_on_precompile = true
   end
 end
