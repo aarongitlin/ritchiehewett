c9aa9075b59de67a43f36743395dcd92f8f28add
Aaron Gitlin <aarongitlin@gmail.com>
2012-11-15 12:49:39 +1100

    removed menu selected class from app helper, added jQuery pageless thing. TODO: move out of application helper into testimonials helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
-# def active_class?(path)
-#   "active_menu" if current_page?(path)
-# end
+  def pageless(total_pages, url=nil, container=nil)
+    opts = {
+      :totalPages => total_pages,
+      :url => '/testimonials/',
+      # :loaderMsg => 'Loading more results',
+      :loaderImage => image_path("loading.gif")
+    }
+    
+    container && opts[:container] ||= container
+    
+    javascript_tag("$(document).ready(function() { 
+                      $('#testimonials').pageless( #{opts.to_json}
+                      );
+                    });")
+  end
 end
