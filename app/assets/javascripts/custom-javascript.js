18cea81d4321764710032c73ce2131f1b217a5f9
Aaron Gitlin <aarongitlin@gmail.com>
2012-11-22 19:58:41 +1100

    javascript for fade effect, including with turbolinks

--- app/assets/javascripts/custom-javascript.js
+++ app/assets/javascripts/custom-javascript.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
     interval: 7000,
     })
 });
+
+$(document).on('page:fetch', function() {
+  $('#mask').fadeIn(600);
+});
+
+// $(document).ready(function() {
+//   $('section#body_content').hide().fadeIn('800');
+//   $('#page_loading').hide();
+// });
+$(function(){
+  var bodyWidth = $('section#body_content').width();
+  var bodyHeight = $('section#body_content').height();
+  $('#mask').show();
+  $('#mask').width(bodyWidth + 30);
+  $('#mask').height(bodyHeight);
+  $('#mask').fadeOut(1200);
+});
+
+$(function(){
+  $('input[type="submit"]').addClass('btn');
+});
